4437a1f3a7e93f5b159695f7ce3d707388d1065b
pavankumar130 <20bcs130@iiitdmj.ac.in>
2023-06-18 10:21:37 +0530

    first commit

---
+++ starter/src/SingleItem.jsx
@@ -0,0 +1,33 @@
+import { useDeleteTasks, useEditTasks } from './reactQueryCustomHooks'
+
+const SingleItem = ({ item }) => {
+  const { editTask } = useEditTasks()
+  const { deleteTask, deleteTaskLoading } = useDeleteTasks()
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={item.isDone}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.isDone })}
+      />
+      <p
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: item.isDone && 'line-through',
+        }}
+      >
+        {item.title}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        disabled={deleteTaskLoading}
+        onClick={() => deleteTask(item.id)}
+      >
+        delete
+      </button>
+    </div>
+  )
+}
+export default SingleItem
